2ae66ed1f587035829a3589a89e005d0842c26d9
Lee,YoungHoon <nobel6018@gmail.com>
2021-08-29 01:44:44 +0900

    feat: create Todo

--- frontend/src/todo/page/index.tsx
+++ frontend/src/todo/page/index.tsx
@@ -6,8 +6,56 @@ import { DeleteButton } from "../element/DeleteButton";
 import { TodoStatus } from "../domain/TodoStatus";
 
 function TodoMain() {
-    const [todos, setTodos] = useState<Todo[]>([]);
+    const [todos, setTodos] = useState<Array<Todo>>([]);
     const [doneTodoIds, setDoneTodoIds] = useState<Array<number>>([]);
+    const [content, setContent] = useState<string>("");
+
+    useEffect(() => {
+        loadAllTodos();
+    }, []);
+
+    return (
+        // bg
+        <div className="bg-gray-200 text-gray-800 flex items-center justify-center h-screen p-3.5">
+            {/*wrapper*/}
+            <div className="container px-3 max-w-md mx-auto">
+                {/*title*/}
+                <div className="flex">
+                    <input
+                        className="border border-gray-800 focus:border-blue-500 rounded w-full py-2 px-3 mr-4 text-black"
+                        placeholder="Add Todo"
+                        value={content}
+                        onChange={(e) => setContent(e.target.value)}
+                    />
+                    <AddButton onClick={() => createTodo(content)} />
+                </div>
+
+                {/* Todo List */}
+                <div>
+                    <ul className="mt-4">
+                        {todos.map((todo) => (
+                            <li key={todo.id} className="flex items-center justify-between mt-3 ">
+                                <div className="flex items-center">
+                                    <input
+                                        type="checkbox"
+                                        name=""
+                                        checked={doneTodoIds.includes(todo.id)}
+                                        onChange={() => updateTodoStatus(todo.id, getInverseStatus(todo.status))}
+                                    />
+                                    {todo.status === TodoStatus.DONE ? (
+                                        <div className="ml-3 font-semibold line-through">{todo.content}</div>
+                                    ) : (
+                                        <div className="ml-3 font-semibold">{todo.content}</div>
+                                    )}
+                                    <DeleteButton onClick={() => deleteTodo(todo.id)} />
+                                </div>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
 
     function loadAllTodos() {
         api.getTodos().then((res) => {
@@ -28,50 +76,16 @@ function TodoMain() {
         });
     }
 
-    useEffect(() => {
-        loadAllTodos();
-    }, []);
-
-    return (
-        // bg
-        <div className="bg-gray-200 text-gray-800 flex items-center justify-center h-screen p-3.5">
-            {/*wrapper*/}
-            <div className="container px-3 max-w-md mx-auto">
-                {/*title*/}
-                <div className="flex">
-                    <input
-                        className="border border-gray-800 focus:border-blue-500 rounded w-full py-2 px-3 mr-4 text-black"
-                        placeholder="Add Todo"
-                    />
-                    <AddButton />
-                </div>
-
-                {/* Todo List */}
-                <div>
-                    <ul className="mt-4">
-                        {todos.map((todo) => (
-                            <li key={todo.id} className="flex items-center justify-between mt-3 ">
-                                <div className="flex items-center">
-                                    <input
-                                        type="checkbox"
-                                        name=""
-                                        checked={doneTodoIds.includes(todo.id)}
-                                        onChange={() => updateTodoStatus(todo.id, inverseStatus(todo.status))}
-                                    />
-                                    {todo.status === TodoStatus.DONE ? (
-                                        <div className="capitalize ml-3 font-semibold line-through">{todo.content}</div>
-                                    ) : (
-                                        <div className="capitalize ml-3 font-semibold">{todo.content}</div>
-                                    )}
-                                    <DeleteButton onClick={() => deleteTodo(todo.id)} />
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </div>
-    );
+    function createTodo(content: string) {
+        if (content === "") {
+            alert("Content should not be empty");
+            return;
+        }
+        api.createTodo({ content }).then(() => {
+            setContent("");
+            loadAllTodos();
+        });
+    }
 
     function updateTodoStatus(id: number, status: TodoStatus) {
         api.updateTodoStatus(id, { status }).then(() => {
@@ -92,7 +106,7 @@ function TodoMain() {
         });
     }
 
-    function inverseStatus(status: TodoStatus) {
+    function getInverseStatus(status: TodoStatus) {
         return status === TodoStatus.DONE ? TodoStatus.NOT_YET : TodoStatus.DONE;
     }
 
